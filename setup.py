54d7d3eac3b2f60e4270b3f974f30220dafce9e4
Parav Syal <paravsyal02@gmail.com>
2025-02-06 21:54:09 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='GEN AI Project',
+    version='0.0.0',
+    author='Parav Syal',
+    author_email='paravsyal02@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
